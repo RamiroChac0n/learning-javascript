66149be7d1b5f1f711642046c10c7e3b715eac8d
Ramiro Chacón <ramiro20010202@gmail.com>
2022-11-24 10:35:47 -0600

    Termina dia 1

---
+++ Day1.js
@@ -0,0 +1,46 @@
+//  1.  Los comentarios pueden hacer que el codigo sea legible.
+//  2.  Bienvenido a 30DaysOfJavaScript.
+
+/*
+    3.  Los comentarios pueden hacer el codigo
+        legible, facil de reutilizar e
+        informativo.
+*/
+
+//  4.  Variables:
+var variableString = 'String';
+var variableBoolean = true;
+var variableUndefined;
+var VariableNull = null;
+
+//  5.
+console.log(typeof(variableString));
+console.log(typeof(variableBoolean));
+console.log(typeof(variableUndefined));
+console.log(typeof(VariableNull));
+
+//  6.  
+var variable1;
+var variable2;
+var variable3;
+var variable4;
+
+//  7.
+var variable5 = 'valor 1';
+var variable6 = 'valor 2';
+var variable7 = 'valor 3';
+var variable8 = 'valor 4';
+
+//  8. Mi informacion personal en varias lineas.
+var myFirstName = 'Ramiro';
+var myLastName = 'Chacon';
+var myMaritalStatus = 'Single'
+var myCountry = 'Guatemala';
+var myAge = 21;
+
+//  9.  Mi informacion personal en una sola linea.
+var yourFirstName = 'Angelica',yourLastName = 'Chacon',yourMaritalStatus = 'Single',yourCountry = 'Guatemala',yourAge = 72;
+
+//  10.
+console.log('I am ' + myAge + ' years old.');
+console.log('You are ' + yourAge + ' years old.');
