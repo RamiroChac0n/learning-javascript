ea3a5e8213b76fdcef4d28e4666af156cea89d01
Ramiro Chacón <ramiro20010202@gmail.com>
2022-11-26 11:21:13 -0600

    Termina inciso 5

--- Day3/Level2.js
+++ Day3/Level2.js
@@ -1,5 +1,5 @@
 //  1.
-let base = prompt('Ingrese la base del triangulo');
+/*let base = prompt('Ingrese la base del triangulo');
 let altura = prompt('Ingrese la altura del triangulo');
 console.log(`El area del triangulo es: ${0.5 * altura * base}`);
 //  2.
@@ -15,3 +15,12 @@ console.log(`El area del rectangulo es: ${largo * ancho}, su perimetro es: ${2 *
 const PI = Math.PI;
 let radio = parseInt(prompt('Ingrese el radio del circulo'));
 console.log(`El area del circulo es: ${PI * radio * radio}, su perimetro es: ${2 * PI * radio}`);
+//  5.*/
+function f (x){
+    y = 2*x -2;
+    return y;
+}
+let interseccionX = 2/2;
+console.log(`La interseccion en X es: ${interseccionX}`);
+console.log(`La interseccion en y es: ${f(0)}`);
+console.log(`La pendiente es: ${(f(1) - f(0))/(1 - 0)}`);
