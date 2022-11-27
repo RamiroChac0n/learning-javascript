f9e776c719f77091340098427e156fa28b83220f
Ramiro Chacón <ramiro20010202@gmail.com>
2022-11-27 09:40:57 -0600

    Completa Level 2

--- Day4/Level2.js
+++ Day4/Level2.js
@@ -6,7 +6,7 @@ switch(true){
     case puntaje > 59: alert('Usted obtuvo una C');break;
     case puntaje > 49: alert('Usted obtuvo una D');break;
     case puntaje > 0: alert('Usted obtuvo una F');break;
-}*/
+}
 //  2.
 let mes = prompt('Que mes es?');
 switch(mes.toLowerCase()){
@@ -14,4 +14,11 @@ switch(mes.toLowerCase()){
     case 'diciembre': case 'enero': case 'febrero':alert('La temporada es Invierno.');break;
     case 'marzo': case 'abril': case 'mayo':alert('La temporada es Primavera.');break;
     case 'junio': case 'julio': case 'agosto':alert('La temporada es Verano.');break;
+}*/
+//  3.
+let hoy = prompt('Que dia es hoy?');
+switch(hoy.toLowerCase()){
+    case 'sabado':case 'domingo':alert(`El ${hoy} es fin de semana.`);break;
+    case 'lunes': case 'martes': case 'miercoles': case 'jueves': case 'viernes':alert(`El ${hoy} es un dia laborable.`);break;
+    default:alert(`${hoy} no es un dia.`);break;
 }
