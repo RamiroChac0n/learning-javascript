51dc2c71e193d4f4d42cf0520f3538e9b658f591
Ramiro Chacón <ramiro20010202@gmail.com>
2022-11-27 09:33:34 -0600

    Inciso 2

--- Day4/Level2.js
+++ Day4/Level2.js
@@ -1,9 +1,17 @@
 //  1.
-let puntaje = prompt('Ingrese su puntaje');
+/*let puntaje = prompt('Ingrese su puntaje');
 switch(true){
     case puntaje > 79: alert('Usted obtuvo una A');break;
     case puntaje > 69: alert('Usted obtuvo una B');break;
     case puntaje > 59: alert('Usted obtuvo una C');break;
     case puntaje > 49: alert('Usted obtuvo una D');break;
     case puntaje > 0: alert('Usted obtuvo una F');break;
+}*/
+//  2.
+let mes = prompt('Que mes es?');
+switch(mes.toLowerCase()){
+    case 'septiembre': case 'octubre': case 'noviembre':alert('La temporada es Otoño.');break;
+    case 'diciembre': case 'enero': case 'febrero':alert('La temporada es Invierno.');break;
+    case 'marzo': case 'abril': case 'mayo':alert('La temporada es Primavera.');break;
+    case 'junio': case 'julio': case 'agosto':alert('La temporada es Verano.');break;
 }
