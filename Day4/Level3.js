33f663c99ff04655a4afc6c4badbd34932412384
Ramiro Chacón <ramiro20010202@gmail.com>
2022-11-27 12:16:19 -0600

    Termina inciso 2

--- Day4/Level3.js
+++ Day4/Level3.js
@@ -1,7 +1,20 @@
 //  1.
-let mes = prompt('Ingrese el mes');
-switch(mes.toLowerCase()){
-    case 'enero':case 'marzo':case 'mayo':case 'julio':case 'agosto':case 'octubre':case 'diciembre':alert(`${mes} tiene 31 dias.`);break;
-    case 'abril':case 'junio':case 'septiembre':case 'noviembre':alert(`${mes} tiene 30 dias.`);break;
-    case 'febrero':alert(`${mes} tiene 28 dias.`);break;
-}
+/*let mes1 = prompt('Ingrese el mes');
+switch(mes1.toLowerCase()){
+    case 'enero':case 'marzo':case 'mayo':case 'julio':case 'agosto':case 'octubre':case 'diciembre':alert(`${mes1} tiene 31 dias.`);break;
+    case 'abril':case 'junio':case 'septiembre':case 'noviembre':alert(`${mes1} tiene 30 dias.`);break;
+    case 'febrero':alert(`${mes1} tiene 28 dias.`);break;
+};*/
+//  2.
+let mes2 = prompt('Ingrese el mes');
+switch(mes2.toLowerCase()){
+    case 'enero':case 'marzo':case 'mayo':case 'julio':case 'agosto':case 'octubre':case 'diciembre':alert(`${mes2} tiene 31 dias.`);break;
+    case 'abril':case 'junio':case 'septiembre':case 'noviembre':alert(`${mes2} tiene 30 dias.`);break;
+    case 'febrero':
+        if(confirm('El año es bisiesto?')){
+            alert(`${mes2} tiene 29 dias.`);
+        }else{
+            alert(`${mes2} tiene 28 dias`);
+        };
+        break;
+};
