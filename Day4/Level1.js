3bd7f19f9531f191a4b40a5e354039dc5f4f4eb4
Ramiro Chacón <ramiro20010202@gmail.com>
2022-11-26 18:46:54 -0600

    Termina inciso 1

--- Day4/Level1.js
+++ Day4/Level1.js
@@ -1,1 +1,5 @@
 //  1.
+let now = new Date();
+let anioNacimiento = prompt('Introduzca el año de nacimiento:');
+let edad = now.getFullYear() - anioNacimiento;
+alert(`Tienes ${edad}, ${edad >= 18 ? `Tienes la edad suficiente para conducir.` : `Podrás conducir después de ${18 - edad} años.`}`);
