0566252ea685226e9631d74154e154acba60d2c3
Ramiro Chacón <ramiro20010202@gmail.com>
2022-11-27 12:17:24 -0600

    Completa dia 4

--- Day4/Level3.js
+++ Day4/Level3.js
@@ -1,10 +1,10 @@
 //  1.
-/*let mes1 = prompt('Ingrese el mes');
+let mes1 = prompt('Ingrese el mes');
 switch(mes1.toLowerCase()){
     case 'enero':case 'marzo':case 'mayo':case 'julio':case 'agosto':case 'octubre':case 'diciembre':alert(`${mes1} tiene 31 dias.`);break;
     case 'abril':case 'junio':case 'septiembre':case 'noviembre':alert(`${mes1} tiene 30 dias.`);break;
     case 'febrero':alert(`${mes1} tiene 28 dias.`);break;
-};*/
+};
 //  2.
 let mes2 = prompt('Ingrese el mes');
 switch(mes2.toLowerCase()){
