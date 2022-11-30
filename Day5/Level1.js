5c26d2b829d9f33820c92155475526f3f6922470
Ramiro Chacón <ramiro20010202@gmail.com>
2022-11-30 11:42:38 -0600

    Termina incisos 15-16

--- Day5/Level1.js
+++ Day5/Level1.js
@@ -49,3 +49,7 @@ if(itCompanies.includes(empresaBuscar)){
 //  14.
 let empresasFiltro = itCompanies.toString();
 console.log(empresasFiltro.match(/[a-np-z]+\o{2}[a-np-z]+|[a-np-z]+\o[a-np-z]+\o[a-np-z]+/gi));
+//  15.
+console.log(itCompanies.sort());
+//  16.
+console.log(itCompanies.reverse());
