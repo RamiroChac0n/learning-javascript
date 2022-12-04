9645e163fb4b6a77b9a7aa282c42a82974144319
Ramiro Chacón <ramiro20010202@gmail.com>
2022-12-04 11:38:36 -0600

    Termina inciso 3

--- Day5/Level2/main.js
+++ Day5/Level2/main.js
@@ -8,3 +8,14 @@ text = text.replace(/[\.\,]+/g,'');
 let words = text.split(' ');
 console.log(words);
 console.log(words.length);
+//  3.
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+console.log(shoppingCart);
+shoppingCart.unshift('Meat');
+console.log(shoppingCart);
+shoppingCart.push('Sugar');
+console.log(shoppingCart);
+shoppingCart.splice(shoppingCart.indexOf('Honey'),1)
+console.log(shoppingCart);
+shoppingCart[shoppingCart.indexOf('Tea')] = 'Green Tea';
+console.log(shoppingCart);
