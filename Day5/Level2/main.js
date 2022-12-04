55409299bac39ae25ea432596892d3f4e0edd633
Ramiro Chacón <ramiro20010202@gmail.com>
2022-12-04 11:19:14 -0600

    Termina inciso 2

--- Day5/Level2/main.js
+++ Day5/Level2/main.js
@@ -1,3 +1,10 @@
 //  1.
 console.log(countries);
 console.log(webTechs);
+//  2.
+let text =
+  "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+text = text.replace(/[\.\,]+/g,'');
+let words = text.split(' ');
+console.log(words);
+console.log(words.length);
