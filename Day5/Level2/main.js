46e3140072ddc915a64f054929ab55d28a16f619
Ramiro Chacón <ramiro20010202@gmail.com>
2022-12-04 11:50:07 -0600

    Termina inciso 5

--- Day5/Level2/main.js
+++ Day5/Level2/main.js
@@ -26,3 +26,10 @@ if(countries.indexOf('Ethiopia') == -1){
 }else{
     console.log('ETHIOPIA');
 };
+//  5.
+if(webTechs.indexOf('Sass') == -1){
+    webTechs.push('Sass');
+    webTechs.sort();
+}else{
+    console.log('Sass es un preproceso de CSS');
+};
