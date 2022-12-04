b29365109d9c639d2a734b584f928be93a1e2f86
Ramiro Chacón <ramiro20010202@gmail.com>
2022-12-04 16:34:25 -0600

    Termina inciso 2

--- Day5/Level3/main.js
+++ Day5/Level3/main.js
@@ -21,3 +21,10 @@ console.log(`Comparando valores: ${Math.abs(edadMinima - edadPromedio)} y ${Math
 
 /* Usar la extension Live Server de vscode */
 console.log(countries.slice(0,10));
+//  2.
+if(countries.length%2 == 0){
+    console.log(`Los paises intermedios son: ${countries[Math.round(countries.length/2 -1)]} y ${countries[Math.round(countries.length/2)]}`);
+}else{
+    console.log(`El pais intermedio es: ${countries[Math.round(countries.length/2 -1)]}`);
+};
+console.log(countries.length);
