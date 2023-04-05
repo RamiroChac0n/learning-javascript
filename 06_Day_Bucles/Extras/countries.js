0d2dfdfb3f3014f3ce1a16e2f5e8ad486c31da1b
Ramiro Chacon <ramiro20010202@gmail.com>
2023-04-05 02:53:11 +0000

    Agrega los extras necesarios para los ejercicios

---
+++ 06_Day_Bucles/Extras/countries.js
@@ -0,0 +1,15 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+// Haz que countries pueda ser llamado desde cualquier archivo
+export {countries};
