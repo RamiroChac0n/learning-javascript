07b482065f7d829f5e253da4ffe1007339b7d016
Ramiro <ramiro20010202@gmail.com>
2023-04-07 04:13:04 +0000

    Corrige el enunciado del ejercicio

---
+++ 06_Day_Bucles/02_Level/06/Description.md
@@ -0,0 +1,15 @@
+Use the countries array to create the following array of arrays:
+  [
+  ['Albania', 'ALB', 7],
+  ['Bolivia', 'BOL', 7],
+  ['Canada', 'CAN', 6],
+  ['Denmark', 'DEN', 7],
+  ['Ethiopia', 'ETH', 8],
+  ['Finland', 'FIN', 7],
+  ['Germany', 'GER', 7],
+  ['Hungary', 'HUN', 7],
+  ['Ireland', 'IRE', 7],
+  ['Iceland', 'ICE', 7],
+  ['Japan', 'JAP', 5],
+  ['Kenya', 'KEN', 5]
+]
--- 06_Day_Bucles/02_Level/06/Solution.js
+++ 06_Day_Bucles/02_Level/06/Solution.js
@@ -1,20 +1,3 @@
-/*
-Use the countries array to create the following array of arrays:
-  [
-  ['Albania', 'ALB', 7],
-  ['Bolivia', 'BOL', 7],
-  ['Canada', 'CAN', 6],
-  ['Denmark', 'DEN', 7],
-  ['Ethiopia', 'ETH', 8],
-  ['Finland', 'FIN', 7],
-  ['Germany', 'GER', 7],
-  ['Hungary', 'HUN', 7],
-  ['Ireland', 'IRE', 7],
-  ['Iceland', 'ICE', 7],
-  ['Japan', 'JAP', 5],
-  ['Kenya', 'KEN', 5]
-]
-*/
 const countries = [
     'Albania',
     'Bolivia',
