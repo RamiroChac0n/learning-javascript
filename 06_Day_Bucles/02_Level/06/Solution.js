ad2b7194edb0107b7c478abdc6490648aa5bc37a
Ramiro <ramiro20010202@gmail.com>
2023-04-07 04:11:44 +0000

    Termina inciso 6

---
+++ 06_Day_Bucles/02_Level/06/Solution.js
@@ -0,0 +1,37 @@
+/*
+Use the countries array to create the following array of arrays:
+  [
+  ['Albania', 'ALB', 7],
+  ['Bolivia', 'BOL', 7],
+  ['Canada', 'CAN', 6],
+  ['Denmark', 'DEN', 7],
+  ['Ethiopia', 'ETH', 8],
+  ['Finland', 'FIN', 7],
+  ['Germany', 'GER', 7],
+  ['Hungary', 'HUN', 7],
+  ['Ireland', 'IRE', 7],
+  ['Iceland', 'ICE', 7],
+  ['Japan', 'JAP', 5],
+  ['Kenya', 'KEN', 5]
+]
+*/
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+let newArray = [];
+let index = 0;
+while (index < countries.length) {
+    newArray[index] = [countries[index], countries[index].substring(0,3).toUpperCase(), countries[index].length];
+    index++;
+}
+console.log(newArray);
