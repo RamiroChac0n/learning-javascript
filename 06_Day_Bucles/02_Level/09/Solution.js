fb22b4e3ab9ad570fcd97edf3790717804e92c0d
Ramiro Chacon <ramiro20010202@gmail.com>
2023-04-09 16:58:44 +0000

    Corrige El inciso 09

--- 06_Day_Bucles/02_Level/09/Solution.js
+++ 06_Day_Bucles/02_Level/09/Solution.js
@@ -12,23 +12,12 @@ const countries = [
     'Japan',
     'Kenya'
   ]
-let countriesIA = [];
-for (let index = 0; index < countries.length; index++) {
-    if (countries[index].includes('ia')) {
-        countriesIA.push(countries[index]);
-    }
-}
-if (countriesIA.length == 0) {
-    console.log('These are countries ends without ia');
-} else {
-    console.log(countriesIA);
-}
 
 // Exercise 09
 let longCountrie = '';
-for (let index = 0; index < countriesIA.length; index++) {
-    if (longCountrie.length < countriesIA[index].length) {
-        longCountrie = countriesIA[index];
+for (let index = 0; index < countries.length; index++) {
+    if (longCountrie.length < countries[index].length) {
+        longCountrie = countries[index];
     }
 }
 console.log(longCountrie);
