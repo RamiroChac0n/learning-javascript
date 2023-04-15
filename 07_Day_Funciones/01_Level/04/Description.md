370c0c16adcb471960fe3fbf0dbaadbd37bfe32e
Ramiro Chacon <ramiro20010202@gmail.com>
2023-04-15 00:48:51 +0000

    Termina inciso 04

---
+++ 07_Day_Funciones/01_Level/04/Description.md
@@ -0,0 +1,2 @@
+# Exercise 04
+An area of a rectangle is calculated as follows: `area = length x width`. Write a function which calculates `areaOfRectangle()`.
---
+++ 07_Day_Funciones/01_Level/04/Solution.js
@@ -0,0 +1,6 @@
+function perimeterOfRectangle(length, width){
+    area = length * width;
+    return area;
+}
+
+console.log(`Area is: ${perimeterOfRectangle(4,3)}`);
